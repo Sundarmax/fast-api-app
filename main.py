de09d3864955494f9948768c180f4d4646ef5d4c
sundar max <sundar.info22@gmail.com>
2026-09-02 15:10:07 +0530

    Depency injection update

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from model import UserCreate, UserResponse, ProductCreate
 
 app = FastAPI()
 
+def get_db():
+    return "Database Connection"
+
+def get_pagination(
+    page: int=1,
+    limit: int=10
+):
+    return {
+        "page": page,
+        "limit": limit
+    }
+
 @app.get("/")
 def home(): #handler
     return { "name": "hello"}
+
+@app.get("/page")
+def get_data(
+    pagination = Depends(get_pagination)
+):
+    return pagination
 
 @app.get("/users/{user_id}", response_model=UserResponse) #path parameters & limit fields in response.
 def get_user(user_id: int):
@@ -31,9 +49,9 @@ def create_user(user : UserCreate):
     return user
 
 @app.get("/products")
-def get_products():
+def get_products(db = Depends(get_db)): #DI - Separation of concerns and reusability.
     return {
-        "Should return all the products"
+        "db" : db
     }
 
 @app.get("/products/{product_id}")
